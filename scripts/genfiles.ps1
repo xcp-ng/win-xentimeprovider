f363a26325d88b09e034fd1067cb84120a779df9
Tu Dinh <ngoc-tu.dinh@vates.tech>
2025-11-24 12:05:15 +0100

    Add versioning and signer scripts
    Signed-off-by: Tu Dinh <ngoc-tu.dinh@vates.tech>

---
+++ scripts/genfiles.ps1
@@ -0,0 +1,51 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory)]
+    [string]$ProjectDir
+)
+
+. "$ProjectDir\..\branding.ps1"
+. "$ProjectDir\..\scripts\branding-generic.ps1"
+
+$BrandingFile = "$ProjectDir\Branding.inc"
+$OldBranding = Get-Content -Raw $BrandingFile -ErrorAction Ignore
+$NewBranding = @"
+VS_VERSION_INFO VERSIONINFO
+ FILEVERSION $((Get-PackageVersion XenTimeProvider).ToString().Replace(".", ","))
+ PRODUCTVERSION $((Get-PackageVersion Product).ToString().Replace(".", ","))
+ FILEFLAGSMASK 0x3fL
+#ifdef _DEBUG
+ FILEFLAGS 0x1L
+#else
+ FILEFLAGS 0x0L
+#endif
+ FILEOS 0x40004L
+ FILETYPE 0x1L
+ FILESUBTYPE 0x0L
+BEGIN
+    BLOCK "StringFileInfo"
+    BEGIN
+        BLOCK "200004b0"
+        BEGIN
+            VALUE "CompanyName", "${Env:VENDOR_NAME}"
+            VALUE "FileDescription", "Xen PV time provider"
+            VALUE "FileVersion", "$(Get-PackageVersion XenTimeProvider)"
+            VALUE "InternalName", "xentimeprovider.dll"
+            VALUE "LegalCopyright", "${Env:COPYRIGHT}"
+            VALUE "OriginalFilename", "xentimeprovider.dll"
+            VALUE "ProductName", "${Env:PRODUCT_NAME}"
+            VALUE "ProductVersion", "$(Get-PackageVersion Product)"
+        END
+    END
+    BLOCK "VarFileInfo"
+    BEGIN
+        VALUE "Translation", 0x2000, 1200
+    END
+END
+
+"@
+
+if ($NewBranding -ne $OldBranding) {
+    Write-Output "Updating Branding.inc"
+    Set-Content -Path $BrandingFile -Value $NewBranding -NoNewline
+}
---
+++ scripts/sign.ps1
@@ -0,0 +1,15 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory)]
+    [string]$ProjectDir,
+    [Parameter(Mandatory)]
+    [string]$Configuration,
+    [Parameter(Mandatory)]
+    [string]$Platform
+)
+
+. "$ProjectDir\..\branding.ps1"
+. "$ProjectDir\..\scripts\branding-generic.ps1"
+. "$ProjectDir\..\scripts\sign.ps1"
+
+Set-SignerFileSignature -FilePath "$ProjectDir\$Platform\$Configuration\xentimeprovider.dll"
